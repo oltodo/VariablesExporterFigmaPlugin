a83966ed424ea6cc84b30c7e9f3a042d51135067
Nicolas Bazille <oltodo@msn.com>
2025-04-24 21:51:09 +0200

    feat: add refresh button

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,12 @@ import { SettingsForm } from '@/components/settings-form'
 import { Button } from '@/components/ui/button'
 import { Drawer, DrawerContent, DrawerHeader, DrawerTitle, DrawerTrigger } from '@/components/ui/drawer'
 import { Textarea } from '@/components/ui/textarea'
-import { DownloadIcon, SettingsIcon } from 'lucide-react'
+import { Tooltip, TooltipContent, TooltipTrigger } from '@/components/ui/tooltip'
+import { Transformer } from '@/lib/transformer'
+import { DownloadIcon, Loader2Icon, RefreshCcwIcon, SettingsIcon } from 'lucide-react'
 import { useCallback, useEffect, useRef, useState } from 'react'
 import useSWR from 'swr'
-import { Transformer } from './lib/transformer'
+import { useDebouncedCallback } from 'use-debounce'
 
 function download(data: string) {
   const blob = new Blob([data], { type: 'application/json' })
@@ -19,6 +21,7 @@ function download(data: string) {
 
 function App() {
   const [data, setData] = useState<Plugin.Data | null>(null)
+  const [isLoading, setIsLoading] = useState(true)
   const [areSettingsOpen, setAreSettingsOpen] = useState(false)
   const [settings, setSettings] = useState<SettingsSchema>({
     resolveAliases: false,
@@ -26,13 +29,21 @@ function App() {
   })
   const textareaRef = useRef<HTMLTextAreaElement>(null)
 
+  const stopLoading = useDebouncedCallback(() => {
+    setIsLoading(false)
+  }, 1000)
+
   const loadData = useCallback(() => {
+    setIsLoading(true)
     parent.postMessage({ pluginMessage: { type: 'get-data' } }, '*')
   }, [])
 
   // Initial load
   useEffect(() => {
-    loadData()
+    const initiateLoad = () => {
+      loadData()
+    }
+    initiateLoad()
   }, [loadData])
 
   useEffect(() => {
@@ -42,6 +53,7 @@ function App() {
       switch (message.type) {
         case 'data':
           setData(message.data as Plugin.Data)
+          stopLoading()
           break
       }
     }
@@ -73,8 +85,25 @@ function App() {
   return (
     <div className="p-4 h-full grid grid-cols-1 grid-rows-[auto_1fr_auto] gap-4">
       <div className="flex justify-end gap-3 items-center">
+        <Tooltip>
+          <TooltipTrigger asChild>
+            <Button
+              size="sm"
+              variant="outline"
+              onClick={loadData}
+              disabled={isLoading}
+              className="mr-auto"
+            >
+              {isLoading ? <Loader2Icon size={14} className="animate-spin" /> : <RefreshCcwIcon size={14} />}
+            </Button>
+          </TooltipTrigger>
+          <TooltipContent side="right">
+            <p>Refresh</p>
+          </TooltipContent>
+        </Tooltip>
+
         <Button size="sm" onClick={() => data && download(transformedData)} disabled={!data}>
-          <DownloadIcon />
+          <DownloadIcon size={14} />
           Download
         </Button>
 
@@ -102,6 +131,7 @@ function App() {
             <SettingsForm
               defaultValues={settings}
               onSubmit={(data) => {
+                loadData()
                 setAreSettingsOpen(false)
                 setSettings(data)
               }}
@@ -111,10 +141,10 @@ function App() {
       </div>
       <Textarea
         ref={textareaRef}
-        className="resize-none font-mono text-sm bg-accent overflow-x-scroll wrap-normal whitespace-pre"
+        className="resize-none font-mono text-sm bg-accent overflow-x-scroll wrap-normal whitespace-pre disabled:opacity-50"
         placeholder="Loading variables..."
         value={transformedData}
-        // readOnly
+        disabled={isLoading}
         onClick={() => {
           textareaRef.current?.select()
         }}
